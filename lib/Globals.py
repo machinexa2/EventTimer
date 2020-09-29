949bcc2d19bdc9e87e185b13270032c08fcf89d9
machinexa <machinexa@gmail.com>
2020-09-29 23:04:08 +0545

    11:04:08 PM 09/29/2020

--- lib/Globals.py
+++ lib/Globals.py
@@ -3,11 +3,13 @@ from lib.TimeFunctions import seconds_from_time
 # Note: For GMT only if event occurs in GMT+2 then subtract 2 hour, i.e if sth start in 17:00:00 GMT +2 then, set 15:00:00 with timeformat GMT
 
 event_1 = {
-    "event_name":"BsidesBOS CTF",
-    "event_date":"2020-09-26",
-    "event_time":"09:00:00",
-    "event_timeformat":"EST",
+    "event_name":"Pentesterlab subscription end",
+    "event_date":"2020-12-01",
+    "event_time":"18:28:00",
+    "event_timeformat":"GMT",
+    "event_description":"For personal use!",
 }
+
 """
 event_2 = {
     "event_name":"HTB Crossfit Release",
